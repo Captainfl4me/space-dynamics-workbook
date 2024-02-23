2e7ffb3cf087ca62f994ae6b00e871348adfcb1f
Nicolas THIERRY <nicolas.thierry.perso@gmail.com>
2024-02-23 18:27:55 +0000

    add remove for temp merged notebook

--- Scripts/genWE1-7.ps1
+++ Scripts/genWE1-7.ps1
@@ -1,3 +1,4 @@
 nbmerge .\WE1.ipynb .\WE2.ipynb .\WE3.ipynb .\WE4.ipynb .\WE5.ipynb .\WE6.ipynb .\WE7.ipynb > WE1-7.ipynb
 jupyter nbconvert --clear-output --inplace WE1-7.ipynb
 jupyter nbconvert --execute --to pdf WE1-7.ipynb --LatexPreprocessor.title="Workbook 1 Hand-in" --LatexPreprocessor.author_names="Nicolas THIERRY"
+rm WE1-7.ipynb
